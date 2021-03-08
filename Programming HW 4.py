583ca057cc79fd273d28855ba055e733406020af
jcinterrante <jcinterrante@uchicago.edu>
2021-03-07 23:24:01 -0600

    Added unemployment rate and output for high unemployment df

--- Programming HW 4.py
+++ Programming HW 4.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from datetime import date
+import random
+import math
+path = "C:\\Users\\jcinterrante\\Documents\\GitHub\\homework-4-jcinterrante\\"
+
 # The first day of class was January 11th, 2021. Write code that shows how many
 # days have elapsed between then and now, where "now" is the date someone runs
 # your code. (Hint: look up the datetime standard Python library.)
 # from datetime import timedelta
-from datetime import date
-#from random import Random
 
 first_day = date(2021, 1, 11)
 today = date.today()
@@ -18,7 +21,7 @@ print(days_elapsed.days, "days")
 
 # Write code that begins with a Python list, containing some number of integers
 # or floats.
-numbers = [1,2,3,4,5]
+numbers = [1,2,3.0,4,5]
 print(numbers)
 
 # - Multiply each value by 2. (Hint: look up list comprehensions)
@@ -40,7 +43,9 @@ print(random.choice(even_numbers))
 def ValidateString (submission):
     assert (isinstance(submission, str)), "Error: Input not a string"
     assert (len(submission) >= 15), "Error: Input must be over 15 words"
-
+    
+# - Second, split the string into five or less words per line. (Hint: look up 
+# the _join_ string method.)
 def WrapText(submission, n):
     word_list = submission.split()
     op = []
@@ -81,55 +86,57 @@ print(FormatString(user_input, words_per_line))
 # Load and merge the two dataframes on "msa". Explain in a 1-2 line comment
 # which type of join is the appropriate one.
 
-# We should use a left join because we want the merged dataset to contain all the
-# rows of employment, with additional variables matched from labor_force
+# We should use an inner join. We need to calculate unemployment rates, so an 
+# observation is only valid if it has info for both employment and labor force
 
 import pandas as pd
-employment = pd.read_csv("./employment.csv", skipfooter = 3)
-labor_force = pd.read_csv("./labor force.csv", skipfooter = 3)
+employment = pd.read_csv(path + "employment.csv", skipfooter = 3)
+labor_force = pd.read_csv(path + "labor force.csv", skipfooter = 3)
 
-merged = employment.merge(labor_force, how = "left", on = "msa")
+merged = employment.merge(labor_force, how = "inner", on = "msa")
 
 # Look at what happened to the "country", "year", and "month" columns after the
 # merge. Redo the merge to fix the issue.
-# The problem is we have duplicate columns, and pandas appended suffixes.
-merged = employment.merge(labor_force[["msa", "Labor Force"]], how = "left", on = "msa")
-
+# The problem is we have duplicate columns, so pandas appended suffixes.
+merged = employment.merge(labor_force[["msa", "Labor Force"]], how = "inner", on = "msa")
+merged.columns = [x.lower().replace(" ", "_") for x in merged.columns]
+                  
 # Turn the "year" and "month" columns into a proper "date" column, with a 
 # datetime datatype.
 merged["day"] = 1
 merged["date"] = pd.to_datetime(merged[["year", "month", "day"]])
-merged = merged[["country", "msa", "Employment", "Labor Force", "date"]]
+merged = merged[["country", "msa", "employment", "labor_force", "date"]]
 
 # Calculate a new column that shows the unemployment rate for each MSA.
-merged["unemployment_rate"] = merged["Employment"] / merged["Labor Force"]
+merged["unemployment_rate"] = 1-(merged["employment"] / merged["labor_force"])
 
 # Oops! Something is clearly wrong with the Houston MSA. The correct labor
 # force value is 2,733,348, but in reproducible research, we should never 
-#directly modify the raw data, even when there are mistakes. Fix the value 
+# directly modify the raw data, even when there are mistakes. Fix the value 
 # using loc, then recalculate.
+merged = merged.set_index("msa")
+merged.loc["Houston-The Woodlands-Sugar Land, TX", "labor_force"] = 2733348
 
+merged["unemployment_rate"] = 1-(merged["employment"] / merged["labor_force"])
 
 # Use the map method to make a new column that shows the unemployment rate
 # formatted neatly as a percentage, with the % symbol after it. Note that you
 # can do this other ways than with map, but for this question use map.
+def MakePercent(number):
+    if(math.isnan(number)):
+        return number
+    else:
+        return("{:.2%}".format(number))
+
+merged["unemployment_rate_percent"] = list(map(MakePercent, merged["unemployment_rate"]))
 
 # Calculate the average unemployment rate for all of the MSAs. Now use loc to
 # show only the subset of rows for which the unemployment rate is higher than
 # the mean.
+unemployment_rate_mean = merged["unemployment_rate"].mean()
+high_unemployment = merged.loc[merged["unemployment_rate"] > unemployment_rate_mean]
 
 # Write the final dataframe to a new file named "data.csv" and commit it to
 # your repo with your code. Since the dataframe index is not meaningful here,
 # write it without the index.
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+high_unemployment.to_csv(path + "data.csv")
